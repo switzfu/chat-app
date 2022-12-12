5871aa4bac9f67c087f7ea603e4719d66b7ce4fd
Switz Fullante <109143419+switzfu@users.noreply.github.com>
2022-12-13 05:06:10 +0800

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Chat App</h1>
+      <div className="history">
+        <h2>Chats</h2>
+      </div>
+      <div className="current">
+        <h2>Current</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
